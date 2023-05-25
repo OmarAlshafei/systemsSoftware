3eaf337954423a2609a0bc973468f8134bf7d274
OmarAlshafei <omaralshafei02@gmail.com>
2023-05-24 23:16:41 -0400

    DONE

--- HW1/vm.c
+++ HW1/vm.c
@@ -2,15 +2,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ARRAY_SIZE 500
+
 int pas[ARRAY_SIZE];
 
-int base(int BP, int L)
-{
+int base(int BP, int L) {
 int arb = BP; // arb = activation record base
-while ( L > 0) //find base L levels down
-{
+while ( L > 0) { //find base L levels down
 arb = pas[arb];
 L--;
 }
@@ -33,88 +33,115 @@ int main(int argc, char *argv[]) {
     int PC = 0;  
 
     printf("                   PC   BP   SP   stack\n");
-    printf("Initial values:    %d    %d   %d\n", PC, BP, SP);
+    printf("Initial values:    %d    %d   %d\n\n", PC, BP, SP);
     
-    int OP;
+    int op;
     int L;
     int M;
-    char OPname[3];
+    char opName[4];
+    int barCount = 0;
+    int bar[x/3];
     int halt = 1;
+    
     while (halt != 0){
     
-    OP = pas[PC];
+    op = pas[PC];
     L = pas[PC + 1];
     M = pas[PC + 2];
     PC += 3;
     
-    switch(OP) {
+    switch(op) {
 
     case 1:
         SP += 1;
-        M = pas[SP];
-        printf("LIT %d  %d  %d  %d", L, M, PC, BP, SP);
+        pas[SP] = M;
+        strcpy(opName, "LIT");
         break;
 
     case 2:
         switch(M){
             case 0:
-                SP = BP - 1
-                BP = pas(SP + 2)
+                SP = BP - 1;
+                BP = pas[SP + 2];
+                PC = pas[SP + 3];
+                strcpy(opName, "LIT");        
                 break;    
 
             case 1:
-
+                pas[SP-1] = pas[SP-1] + pas[SP];
+                SP = SP - 1;
+                strcpy(opName, "ADD");        
                 break;    
 
             case 2:
-
-                break;    
-
+                pas[SP-1] = pas[SP-1] - pas[SP];
+                SP = SP - 1;
+                strcpy(opName, "SUB");        
+                break;   
+                 
             case 3:
-
+                pas[SP-1] = pas[SP-1] * pas[SP];
+                SP = SP - 1;            
+                strcpy(opName, "MUL");        
                 break;    
 
             case 4:
-
+                pas[SP-1] = pas[SP-1] / pas[SP];
+                SP = SP - 1;            
+                strcpy(opName, "DIV");        
                 break;    
 
             case 5:
-
+                pas[SP-1] = pas[SP-1] == pas[SP];
+                SP = SP - 1;            
+                strcpy(opName, "EQL");        
                 break;    
 
             case 6:
-
+                pas[SP-1] = pas[SP-1] != pas[SP];
+                SP = SP - 1;            
+                strcpy(opName, "NEQ");        
                 break;    
 
             case 7:
-
-                break;    
+                pas[SP-1] = pas[SP-1] < pas[SP];
+                SP = SP - 1;                       
+                strcpy(opName, "LSS");        
+                break; 
+                   
             case 8:
-
+                pas[SP-1] = pas[SP-1] <= pas[SP];
+                SP = SP - 1;                                   
+                strcpy(opName, "LEQ");        
                 break;    
 
             case 9:
-
+                pas[SP-1] = pas[SP-1] > pas[SP];
+                SP = SP - 1;                                   
+                strcpy(opName, "GTR");        
                 break;    
 
             case 10:
-
+                pas[SP-1] = pas[SP-1] >= pas[SP];
+                SP = SP - 1;                                   
+                strcpy(opName, "GEQ");        
                 break;
-
-            
+                
+            default :
+                printf("ERROR");
         }
         break;
     
     case 3:
         SP += 1;
         pas[SP] = pas[base(BP, L) + M];
-        printf("LOD %d  %d  %d  %d", L, M, PC, BP, SP);
+        strcpy(opName, "LOD");
         break;  
 
     case 4:
         pas[base(BP, L) + M] = pas[SP];
         SP -= 1;
-        printf("STO %d  %d  %d  %d", L, M, PC, BP, SP);
+        strcpy(opName, "STO");
         break;      
 	
     case 5:
@@ -122,54 +149,60 @@ int main(int argc, char *argv[]) {
         pas[SP + 2] = BP;
         pas[SP + 3] = PC;
         BP = SP + 1;
-        SP += M;
-        printf("CAL %d  %d  %d  %d", L, M, PC, BP, SP);
+        PC = M;
+        bar[barCount] = BP;
+        barCount++;
+        strcpy(opName, "CAL");
         break;      
         
     case 6:
         SP += M;
-        printf("INC %d  %d  %d  %d", L, M, PC, BP, SP);
+        strcpy(opName, "INC");
         break;              
         
     case 7:
         PC = M;
-        printf("JMP %d  %d  %d  %d", L, M, PC, BP, SP);
+        strcpy(opName, "JMP");
         break;              
 
     case 8:
         if (pas[SP] == 0){
         PC = M;
         }
-        SP = SP-1;
-        printf("JPC %d  %d  %d  %d", L, M, PC, BP, SP);
+        SP -= 1;
+        strcpy(opName, "JPC");
         break;              
 
     case 9:
-        if (M = 1){
-            printf("Output result is: %d", pas[SP]);
+        if (M == 1){
+            printf("Output result is: %d\n", pas[SP]);
             SP -= 1;
         }
-        if (M = 1){
+        if (M == 2){
             SP += 1;
-            scanf("Please Enter an Integer: ")
+            printf("Please Enter an Integer: ");
+            scanf("%d", &pas[SP]);
         }
-        if (M = 1){
+        if (M == 3){
             halt = 0;
-        }    
+        }
+        strcpy(opName, "SYS");
         break;
-          
+        
    default :
+       printf("ERROR");
+    }
    
+    printf("          %s  %d  %2d  %2d  %d  %d  ", opName, L, M, PC, BP, SP);
+
     for (int i = x; i <= SP; i++){
-        for (int j = 0; j <= k; j++)
-            if (i == bar[j])
-                printf("| ");
-            printf("%d ", pas[i]);
+        for (int j = 0; j <= barCount; j++){
+            if (bar[j] == i)
+                printf("|  ");
+        }
+            printf("%d  ", pas[i]);        
     }
-
     printf("\n");
-  }
-}
-    }
-
+    }    
+    return 0;
 }
