bd716562e5e124419a6501c0b4077c74c226191e
OmarAlshafei <omaralshafei02@gmail.com>
2023-07-06 20:43:18 -0400

    added condition expression and term

--- HW3/parsercodegen.c
+++ HW3/parsercodegen.c
@@ -13,17 +13,18 @@
 
 // token values
 typedef enum {
-oddsym = 1, identsym, numbersym, plussym, minussym,
-multsym, slashsym, xorsym, eqsym, neqsym, lessym, leqsym,
-gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-readsym , elsesym} token_type;
+    oddsym = 1, identsym, numbersym, plussym, minussym,
+    multsym, slashsym, xorsym, eqsym, neqsym, lessym, leqsym,
+    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym , elsesym
+} token_type;
 
 typedef struct instructions{
-  char OP[4];
-  int L;
-  int M;
+    char OP[4];
+    int L;
+    int M;
 } instructions;
 
 // token struct 
@@ -48,7 +49,7 @@ typedef struct{
 instructions assembly[MAX_SYMBOL_TABLE_SIZE];
 symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
 token* tokenArr; 
-int sbIndex = 0;       // index of the symbol table
+int tableIndex = 0;    // index of the symbol table
 int curToken;          // stores the current token to analyze
 int maxToken;          // stores the max index of token array
 int curTokenIdx = 0;   // stores the current token to analyze
@@ -66,10 +67,6 @@ int isSpecialSymbol(char c){
     return 0;
 }
 
-int getToken(){
-    return tokenArr[curTokenIdx].token;
-}
-
 
 int program(FILE*);
 int block(FILE*);
@@ -80,32 +77,11 @@ int condition(FILE*);
 int expression(FILE*);
 int term(FILE*);
 int factor(FILE*);
-/*
-// linear search through symbol table looking at name (str)
-// return index if found, -1 if not
-int SYMBOLTABLECHECK(char str[51]){
-
-    for(int i = sbIndex; i >= 0; i--){
-
-        if(strcmp(symbol_table[i].name,str) == 0) return i; 
-
-    }
-
-    return -1;
-}
-
-
-int factor(int token){
-    if(token == identsym){
-        int symIdx = SYMBOLTABLECHECK(token);
-
-    }
-}
-
-*/
-
-
-
+void errorRecovery(FILE*);
+void addTable(int, char*, int, int, int, int);
+int checkTable(int, char*);
+void printTable();
+int getToken(FILE*);
 
 
 // driver function 
@@ -123,7 +99,7 @@ int main(int argc, char *argv[]) {
 
     // local variables 
     char reservedWords[14][20] = {"const", "var", "call", "begin", "end", "if", "then",
-                                  "xor", "while", "do", "read", "write"};
+                                  "xor", "while", "do", "read", "write", "odd"};
     char* inputStr;     // hold the input from file
     char buffer[25];    // temporary string 
     int len;            // length of the file
@@ -198,76 +174,76 @@ int main(int argc, char *argv[]) {
                 tokenArr[tokenIdx].token = varsym; 
             }
 
-            // procedure
-            else if(strcmp(buffer, reservedWords[2]) == 0){
-                strcpy(tokenArr[tokenIdx].type, "procedure");
-                tokenArr[tokenIdx].token = procsym; 
-            }
-
             // call
-            else if(strcmp(buffer, reservedWords[3]) == 0){
+            else if(strcmp(buffer, reservedWords[2]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "call");
                 tokenArr[tokenIdx].token = callsym; 
             }
 
             // begin
-            else if(strcmp(buffer, reservedWords[4]) == 0){
+            else if(strcmp(buffer, reservedWords[3]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "begin");
                 tokenArr[tokenIdx].token = beginsym; 
             }
 
             // end
-            else if(strcmp(buffer, reservedWords[5]) == 0){
+            else if(strcmp(buffer, reservedWords[4]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "end");
                 tokenArr[tokenIdx].token = endsym; 
             }
 
             // if
-            else if(strcmp(buffer, reservedWords[6]) == 0){
+            else if(strcmp(buffer, reservedWords[5]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "if");
                 tokenArr[tokenIdx].token = ifsym; 
             }
 
             // then
-            else if(strcmp(buffer, reservedWords[7]) == 0){
+            else if(strcmp(buffer, reservedWords[6]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "then");
                 tokenArr[tokenIdx].token = thensym; 
             }
 
             // xor
-            else if(strcmp(buffer, reservedWords[8]) == 0){
+            else if(strcmp(buffer, reservedWords[7]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "xor");
                 tokenArr[tokenIdx].token = xorsym; 
             }
 
             // else
-            else if(strcmp(buffer, reservedWords[9]) == 0){
-                strcpy(tokenArr[tokenIdx].type, "else");
-                tokenArr[tokenIdx].token = elsesym; 
-            }
+            // else if(strcmp(buffer, reservedWords[8]) == 0){
+            //     strcpy(tokenArr[tokenIdx].type, "else");
+            //     tokenArr[tokenIdx].token = elsesym; 
+            // }
 
             // while
-            else if(strcmp(buffer, reservedWords[10]) == 0){
+            else if(strcmp(buffer, reservedWords[8]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "while");
                 tokenArr[tokenIdx].token = whilesym; 
             }
 
             // do
-            else if(strcmp(buffer, reservedWords[11]) == 0){
+            else if(strcmp(buffer, reservedWords[9]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "do");
                 tokenArr[tokenIdx].token = dosym; 
             }
 
             // read
-            else if(strcmp(buffer, reservedWords[12]) == 0){
+            else if(strcmp(buffer, reservedWords[10]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "read");
                 tokenArr[tokenIdx].token = readsym; 
             }
 
             // write
-            else if(strcmp(buffer, reservedWords[13]) == 0){
+            else if(strcmp(buffer, reservedWords[11]) == 0){
                 strcpy(tokenArr[tokenIdx].type, "write");
                 tokenArr[tokenIdx].token = writesym; 
+            }
+
+            // odd
+            else if(strcmp(buffer, reservedWords[12]) == 0){
+                strcpy(tokenArr[tokenIdx].type, "odd");
+                tokenArr[tokenIdx].token = oddsym; 
             }
 
             // Error handling
@@ -466,32 +442,103 @@ int main(int argc, char *argv[]) {
             x += sprintf(tokenVar + x, "%s ", tokenArr[i].type);
         }
     }
+
+
+    // ----------------------------------- HW3 -----------------------------------
     
     FILE *ifp = fopen("lexemelist.txt", "w+");
     fputs(tokenVar, ifp);
-    if (program(ifp) == -1){
+    fclose(ifp);
+
+    FILE *rfp = fopen("lexemelist.txt", "r");
+    if (program(rfp) == -1){
         printf("Error");
         fclose(ifp);
         return -1;
     }
     
-    fclose(ifp);
     fclose(fp);
+    fclose(rfp);
     free(tokenVar);
     free(inputStr);
 }
 
+// ----------------------------------- HW3 -----------------------------------
+
+// get the next token
+int getToken(FILE* input){
+
+    if(input != EOF){
+        int temp;
+        fscanf(input, "%d", &temp); // get next token
+        return temp;
+    }
+
+    printf("End of file");
+    return INT32_MIN;
+}
+
+// skip tokens until a “;” is found 
+// return the next token
+void errorRecovery(FILE *input){
+    while(curToken != semicolonsym){
+        curToken = getToken(input);
+
+        if(curToken == INT32_MIN){
+            printf("Error: no more token before finding ;");
+            exit(1);
+        }
+    }
+}
+
+// add a symbol to the symbol table
+void addTable(int kind, char name[10], int val, int level, int address, int mark){
+    symbol_table[tableIndex].kind = kind; 
+    strcpy(symbol_table[tableIndex].name, name);
+    symbol_table[tableIndex].val = val; 
+    symbol_table[tableIndex].level = level; 
+    symbol_table[tableIndex].addr = address; 
+    symbol_table[tableIndex].mark = mark; 
+    tableIndex++;
+}
+
+// check the symbol table for a token kind and name
+// if found then return the index, else return -1
+int checkTable(int kind, char name[10]){
+    for(int i = tableIndex - 1; i > 0; i--){
+        if((symbol_table[i].kind == kind) && (strcmp(symbol_table[i].name, name) == 0))
+            return i; 
+    }
+
+    return -1;
+}
+
+// print the content of the symbol table
+void printTable(){
+    int cur = 1;
+
+    printf("\nSymbol Table: ");
+    printf("\nKind\t\tName\t\tValue\t\tLevel\t\tAddress\t\tMark");
+
+    while(cur < tableIndex){
+        printf("\n%d\t\t%s\t\t%d\t\t%d\t\t%d\t\t%d",symbol_table[cur].kind, symbol_table[cur].name, 
+        symbol_table[cur].val, symbol_table[cur].level, symbol_table[cur].addr, symbol_table[cur].mark);
+        cur++;
+    }
+}
+
+
 int program(FILE *input){
     
     if (block(input) == -1){
-    return -1;
+        return -1;
     }
     if (tokenArr->token != periodsym){
         printf("Error : program must end with period %d %s\n", tokenArr->token, tokenArr->type);
         return -1;
     }
     
-    //emit(9, "SYS", 0, 3); we need to make an emit func
+    // FIXME - emit(9, "SYS", 0, 3); we need to make an emit func
     
     printf("Assembly Code:\n\n");
     printf("Line\tOP\tL\tM\n");
@@ -504,7 +551,7 @@ int program(FILE *input){
     return 0;
 }
 
-int block (FILE *input){
+int block(FILE *input){
     
   int contant = constDeclaration(input);
   int var = varDeclaration(input);
@@ -516,3 +563,202 @@ int block (FILE *input){
   return result;
 }
 
+int constDeclaration(FILE *input){
+
+    curToken = getToken(input);
+
+    if(curToken != constsym) return 0;
+
+    else if(curToken == constsym){
+
+        do{
+            char tempName[10];
+            int tempVal;
+
+            curToken = getToken(input);
+
+            // error handling
+            if(curToken != identsym){
+                printf("Error: const keywords must be followed by identifiers ");
+                exit(1);
+            }
+
+            fscanf(input, "%d", tempName);
+
+            curToken = getToken(input);
+
+            if(curToken != becomessym){
+                printf("Error: constants must be assigned with := ");
+                exit(1);
+            }
+
+            curToken = getToken(input);
+            if(checkTable(1, tempName) == -1){
+                addTable(1, tempName, tempVal, 0, 0, 0);
+            }
+            else{
+                printf("Error: symbol name has already been declared");
+                exit(1);
+            }
+
+            curToken = getToken(input);
+
+        }while(curToken == commasym);
+
+        // error handling
+        if(curToken != semicolonsym){
+            printf("Error: constant declaration must be followed by a semicolon ");
+            errorRecovery(input);
+        }
+
+    }
+}
+
+
+int condition(FILE *input){
+    curToken = getToken(input);
+    
+    if (curToken == oddsym){
+        getToken(input);
+
+    if (expression(input) == -1) 
+        return -1;
+
+    emit(2, "OPR", 0, 6);
+    }
+    else{
+        if (expression(input) == -1) 
+            return -1;
+
+        switch(curToken){
+            case eqsym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 8);
+                break;
+            case neqsym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 9);
+                break;
+            case lessym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 10);
+                break;
+            case leqsym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 11);
+                break;
+            case gtrsym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 12);
+                break;
+            case geqsym:
+                getToken(input);
+                if (expression(input) == -1) 
+                    return -1;
+                emit(2, "OPR", 0, 13);
+                break;
+            default:
+                printf("Error : condition must contain comparison operator");
+                return -1;
+        }
+  }
+
+  return 1;
+}
+
+int expression(FILE *input){
+    
+    curToken = getToken(input);
+
+    if (curToken == minussym){
+        curToken = getToken(input);
+
+        if (term(input) == -1)
+            return -1;
+        
+        emit(2, "OPR", 0, 1);
+
+        while (curToken == plussym || curToken == minussym){
+            if (curToken == plussym){
+                curToken = getToken(input);
+                if (term(input) == -1)
+                    return -1;
+                emit(2, "OPR", 0, 2);
+            }
+            else{
+                curToken = getToken(input);
+                if (term(input) == -1)
+                    return -1;
+                emit(2, "OPR", 0, 3);
+            }
+        }
+    }
+    else{
+        if (curToken == plussym)
+            curToken = getToken(input);
+
+        if (term(input) == -1)
+            return -1;
+
+        while (curToken == plussym || curToken == minussym){
+            if (curToken == plussym){
+                curToken = getToken(input);
+                if (term(input) == -1)
+                    return -1;
+                emit(2, "OPR", 0, 2);
+            }
+            else{
+                curToken = getToken(input);
+                if (term(input) == -1)
+                    return -1;
+                emit(2, "OPR", 0, 3);
+            }
+        }
+    }
+    return 1;
+}
+
+int term(FILE *input){
+    curToken = getToken(input);
+
+  if (factor(input) == -1){
+    return -1;
+  }
+
+  while (curToken == multsym || curToken == slashsym){
+    switch(curToken){
+        
+        case multsym:
+            curToken = getToken(input);
+            if (factor(input) == -1)
+            return -1;
+            emit(2, "OPR", 0, 4);
+            break;
+        
+        case slashsym:
+            curToken = getToken(input);
+            if (factor(input) == -1)
+            return -1;
+            emit(2, "OPR", 0, 5);
+            break;
+        
+        default:
+            printf("Error : Unexpected token %d. Line %d.\n", curToken, lineCount);
+            return -1;
+            
+    }
+
+  }
+
+  return 1;
+}
